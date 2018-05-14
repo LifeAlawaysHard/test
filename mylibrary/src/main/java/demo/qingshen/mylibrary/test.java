87b854e50f1be622b575f15d2eb8c7dbdbde007c
dwb <348281373@qq.com>
2018-05-14 14:22:42 +0800

    Initial commit

---
+++ mylibrary/src/main/java/demo/qingshen/mylibrary/test.java
@@ -0,0 +1,14 @@
+package demo.qingshen.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by yyp on 2018/5/14.
+ */
+
+public class test {
+    public static void ShowToast(Context context) {
+        Toast.makeText(context, "哈哈", Toast.LENGTH_LONG);
+    }
+}
